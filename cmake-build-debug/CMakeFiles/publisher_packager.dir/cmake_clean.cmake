100740d5a221cce8478986f593db7b5a9922c146
Ahmet Lekesiz <ahmetlekesiz@gmail.com>
2021-01-20 00:33:05 +0300

    Implement buffer

---
+++ cmake-build-debug/CMakeFiles/publisher_packager.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/publisher_packager.dir/main.c.o"
+  "CMakeFiles/publisher_packager.dir/producerConsumer.c.o"
+  "CMakeFiles/publisher_packager.dir/queue.c.o"
+  "publisher_packager"
+  "publisher_packager.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/publisher_packager.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+
+int main() {
+    printf("Hello, World!\n");
+    // Init buffer queue
+
+
+    return 0;
+}
+
